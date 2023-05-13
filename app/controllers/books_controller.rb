6fb1010c5d53968ce4b1454c222df22bc06fecbb
Nishimurashun <132205509+sHuN945@users.noreply.github.com>
2023-05-13 14:35:12 +0000

    [update]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,44 @@
 class BooksController < ApplicationController
-  def homes
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      #flash[:notice] = "投稿が成功しました"　フラッシュメッセージ記述場所
+      redirect_to list_path(@book.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Books.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  private
+  def book_params
+    params.require(:list).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get 'books/homes'
-  get 'homes/top'
-  post 'lists' => 'lists#create'
+
+  get '/book' => 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
